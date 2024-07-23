e5bff7df1ea40d68dc073cedc144c54269a8ff3b
Carlos Gimenez Berral <carlos.gimenez@external.wolterskluwer.com>
2024-07-23 20:16:34 +0200

    created update and delete endpoints

--- Web.Api/Endpoints/CustomerEndpoints.cs
+++ Web.Api/Endpoints/CustomerEndpoints.cs
@@ -39,15 +39,46 @@ namespace Web.Api.Endpoints
             {
                 using var connection = sqlconnectionFactory.Create();
 
-                const string sql = """"
+                const string sql = """
                     INSERT INTO Customers (FirstName, LastName, Email, DateOfBirth)
                     VALUES (@FirstName, @LastName, @Email, @DateOfBirth)
-                    """";
+                    """;
 
                 await connection.ExecuteAsync(sql, customer);
 
                 return Results.Ok();
 
+            });
+
+            builder.MapPut("customers/{id}", async (int id, Customer customer, SqlConnectionFactory sqlConnectionFactory) =>
+            {
+                using var connection = sqlConnectionFactory.Create();
+
+                customer.Id = id;
+
+                const string sql = """
+                    UPDATE Customers
+                    SET FirstName = @FirstName,
+                        LastName = @LastName,
+                        Email = @Email,
+                        DateOfBirth = @DateOfBirth
+                    WHERE Id = @Id
+                    """;
+
+                await connection.ExecuteAsync(sql, customer);
+
+                return Results.NoContent();
+            });
+
+            builder.MapDelete("customers/{id}", async (int id, SqlConnectionFactory sqlConnectionFactory) =>
+            {
+                using var connection = sqlConnectionFactory.Create();
+
+                const string sql = "DELETE FROM Customers WHERE Id = @CustomerId";
+
+                await connection.ExecuteAsync(sql, new { CustomerId = id });
+
+                return Results.NoContent();
             });
 
         }
