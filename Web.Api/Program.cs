e1185d1f7cecff96280cf7547800b2418edcdd00
Carlos Gimenez Berral <carlos.gimenez@external.wolterskluwer.com>
2024-07-23 18:55:01 +0200

    created get endpoint to fetch all customers

--- Web.Api/Program.cs
+++ Web.Api/Program.cs
@@ -1,3 +1,7 @@
+using Dapper;
+using Microsoft.Data.SqlClient;
+using Web.Api.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -15,5 +19,18 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapGet("customers", async (IConfiguration configuration) =>
+{
+    var connectionString = configuration.GetConnectionString("DefaultConnection")!;
+
+    using var connection = new SqlConnection(connectionString);
+
+    const string sql = "SELECT Id, FirstName, LastName, Email, DateOfBirth FROM Customers";
+
+    var customers = await connection.QueryAsync<Customer>(sql);
+
+    return Results.Ok(customers);
+});
 
 app.Run();
